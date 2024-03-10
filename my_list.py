674d2eb4e492c62cd7eab2c11d73d0f73c95e323
charlesnyankieya <nyankieyao@gmail.com>
2024-03-10 15:04:31 +0300

    This is the commit of my python lists assignment

---
+++ my_list.py
@@ -0,0 +1,29 @@
+# initialising my_list
+my_list = []
+
+# appending values
+my_list.extend([10, 20, 30, 40])
+
+# inserting 15 in the 1st position
+my_list.insert(1, 15)
+print(my_list)
+
+# extending the list
+my_list.extend([50, 60, 70])
+print(my_list)
+
+# deleting the last item on the list
+
+my_list.pop()
+print(my_list)
+
+# sorting the list in ascending order
+my_list.sort()
+print(my_list)
+
+# finding and printing the position of 30 in the list
+thirty = my_list.index(30)
+print(thirty)
+
+
+
